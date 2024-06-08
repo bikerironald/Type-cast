2b4dc3887130acf4d46cbb13524fa7ab274a295c
Ronarld Bikeri <bikerironald@gmail.com>
2024-06-08 03:05:37 +0300

    Add files via upload
     type casting  which is to convert a data type of a value to another data type

---
+++ type_casting.py
@@ -0,0 +1,25 @@
+# type casting  which is to convert a data type of a value to another data type
+
+"""
+int()
+float()
+str()
+bool()
+list()
+tuple()
+set()
+dict()
+"""
+x = bool(1 )#int()
+y = bool(2.0 ) #float
+z = bool('3' ) #str
+
+x = str(x)
+y = str(y)
+z  = str(z)
+print(type(x ))
+print(type(y ))
+print(type(z ))
+
+result = x  + y  + z  
+print(result)
